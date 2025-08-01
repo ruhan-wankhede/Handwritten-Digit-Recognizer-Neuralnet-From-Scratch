84bb2f039e76078bb80d03e47faec8b0ba7596e2
Ruhan Wankhede <ruhanwankhede0@gmail.com>
2025-08-01 16:22:33 +0530

    simple drawing working

---
+++ main.py
@@ -0,0 +1,76 @@
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 784, 784
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Handwritten Digit Recognizer")
+
+grid = [[0 for i in range(28)] for j in range(28)]
+
+def clear_grid():
+    global grid
+    grid = [[0 for i in range(28)] for j in range(28)]
+
+def draw_gridlines():
+    """
+    draw the gridlines
+    """
+    for i in range(28):
+        pygame.draw.line(WIN, (0, 0, 0), (i * 28, 0), (i * 28, HEIGHT))
+        pygame.draw.line(WIN, (0, 0, 0), (0, i * 28), (WIDTH, i * 28))
+
+def draw_pixels():
+    """
+    draw the pixels of the digit
+    """
+    for x in range(28):
+        for y in range(28):
+            if grid[y][x] == 1:
+                pygame.draw.rect(WIN, (0, 0, 0), (x * 28, y * 28, 28, 28))
+
+
+def update_pixels(mouse_x: float, mouse_y: float):
+    """
+    update the grid
+    """
+    if not int(mouse_y // 28) > 27 and not int(mouse_x // 28) > 27:
+        grid[int(mouse_y // 28)][int(mouse_x // 28)] = 1
+
+
+
+def main():
+    run = True
+    drawing = False
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(240)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                drawing = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                drawing = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    clear_grid()
+
+        WIN.fill((255, 255, 255))
+
+        draw_gridlines()
+
+        if drawing:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            update_pixels(mouse_x, mouse_y)
+
+        draw_pixels()
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
